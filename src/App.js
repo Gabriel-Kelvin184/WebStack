151c7cb9fa03801b4182772f7a84727467bc8f7c
Gabriel-Kelvin184 <141640968+Gabriel-Kelvin184@users.noreply.github.com>
2023-10-11 18:38:00 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import SearchBar from './searchbar';
-import ReviewForm from './reviewform';
-import BookList from './booklist';
-import ReviewList from './reviewlist';
-
-const initialBooks = [
-  { id: 1, title: 'Book 1', author: 'Author 1' },
-  { id: 2, title: 'Book 2', author: 'Author 2' },
-  // Add more book entries here
-];
+import React, { useState } from 'react';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+import './styles.css';
 
 function App() {
-  const [books, setBooks] = useState(initialBooks);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [reviews, setReviews] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-  const addReview = (newReview) => {
-    setReviews([...reviews, newReview]);
+  const addTask = (taskName) => {
+    const newTask = {
+      id: Math.random().toString(),
+      name: taskName,
+    };
+    setTasks([...tasks, newTask]);
   };
 
-  useEffect(() => {
-    // Replace with actual data fetching from an API or database
-    // Example using mock data:
-    setTimeout(() => {
-      setBooks(initialBooks);
-    }, 1000); // Simulate a delay
-  }, []);
+  const editTask = (taskId, newName) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, name: newName } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const filteredTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(filteredTasks);
+  };
 
   return (
-    <div className="App">
-      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <ReviewForm onReviewSubmit={addReview} />
-      <BookList books={books} />
-      <ReviewList reviews={reviews} />
+    <div className="app">
+      <h1>Task Management Application</h1>
+      <TaskForm onSubmit={addTask} />
+      <TaskList tasks={tasks} onDelete={deleteTask} onEdit={editTask} />
     </div>
   );
 }
